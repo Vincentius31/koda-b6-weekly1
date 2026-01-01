7b08ee6474fdd73bc434c65acc10706f9543239b
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2026-01-01 15:09:42 +0700

    handling UI dan halaman utama

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+// inisialisasi penggunaan Readline untuk stdin dan stdout
+const https = require("https");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 let menuMakanan = [] //menyimpan menu makanan
 let kerajang = [] //menyimpan makanan yang akan di checkout
 let historyPembelian = [] //menyimpan history pembelian
 const url = "https://raw.githubusercontent.com/Vincentius31/koda-b6-weekly1/refs/heads/main/data.json" //variable menyimpan url data
-
-//Penggunaan readline untuk menggunakan stdin dan stdout
-const readline = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
 
 //function pengambilan data dengan async function dengan metode fetch serta menggunakan try-catch
 async function ambilData() {
@@ -22,3 +24,37 @@ async function ambilData() {
     }
 }
 
+//tampilan utama pada console box
+function menuUtama(){
+    console.log("\=== Aplikasi Pemesanan Iga Bakar SiJangkung ===")
+    console.log("1. Cari Menu Makanan")
+    console.log("2. Lihat Keranjang Pembelian")
+    console.log("3. History Pembelian")
+    console.log("4. Checkout")
+
+    rl.question("Pilih menu (1-4): ", pindahHalaman)
+}
+
+//penggunaan switch untuk handle halaman utama dengan fungsi untuk memberikan perintah pindah halaman
+function pindahHalaman(pilihan){
+    switch(pilihan){
+        case "1":
+        halamanMenu()
+        break
+
+        case "2":
+        halamanKeranjangPembelian()
+        break
+
+        case "3":
+        halamanHistoryPembelian()
+        break
+
+        case "4":
+        halamanCheckout()
+        break
+
+        default: menuUtama()
+    }
+
+}
