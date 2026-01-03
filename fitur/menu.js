ad5b60df2a27259649cc07ec6e7eed3dacf7ec49
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2026-01-03 17:33:09 +0700

    Penambahan fitur search menu.js
    Pembuatan fitur search pada file menu.js

--- fitur/menu.js
+++ fitur/menu.js
@@ -3,3 +3,22 @@ import {formatHarga} from "../utils/format.js"
 import {menuData} from "../data/menu.js"
 import { addToCart } from "./cart.js"
 
+export async function searchMenu() {
+    console.clear()
+    const keyword = await ask("Masukan kata pencarian: ")
+
+    let found = false
+
+    for(let i = 0; i< menuData.length; i++){
+        if(menuData[i].namaMakanan.toLowerCase().includes(keyword.toLowerCase())){
+            console.log(`${menuData[i].id}. ${menuData[i].namaMakanan} | Stok: ${menuData[i].stok}`)
+            found = true
+        }
+    }
+
+    if(!found){
+        console.log("Menu makanan tidak ditemukan")
+    }
+
+    await ask("\nEnter untuk kembali...")
+}
